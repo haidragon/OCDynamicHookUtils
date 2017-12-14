03170893c206d6199c531640ec61242ada769686
renwei.chen <renwei.chen@icloud.com>
2017-12-14 10:25:27 +0800

    重命名

---
+++ WX_Hook/WX_Hook.h
@@ -0,0 +1,13 @@
+//
+//  WX_Hooker.h
+//  WX_Hooker
+//
+//  Created by renwei.chen on 2017/12/12.
+//  Copyright © 2017年 YY.inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WX_Hook: NSObject
+
+@end
---
+++ WX_Hook/WX_Hook.m
@@ -0,0 +1,30 @@
+//
+//  WX_Hooker.m
+//  WX_Hooker
+//
+//  Created by renwei.chen on 2017/12/12.
+//  Copyright © 2017年 YY.inc. All rights reserved.
+//
+
+#import "WX_Hook.h"
+#import "OCDynamicHookUtils.h"
+#import <Cocoa/Cocoa.h>
+
+@implementation WX_Hook
+
++(void)load{
+    NSString *className = @"MessageService";
+    NSString *selName = @"onRevokeMsg:";
+    BOOL hook = [OCDynamicHookUtils AddHookInstanceMethodImp:^id(void *args, ...) {
+        NSLog(@"防止撤回...");
+        return nil;
+    } toClassName:className toReplaceSelectorName:selName];
+    NSLog(@"Hook !!! 防止撤回 %@\n\n",hook?@"YES":@"NO");
+    if(!hook){
+        Class aClass = NSClassFromString(className);
+        Method method = class_getInstanceMethod(aClass, NSSelectorFromString(selName));
+        NSLog(@"class=%p method=%p\n",aClass,method);
+    }
+}
+
+@end
